674a244c55967efba95d71a77e2d6c4e2b955ad3
Samyak Jain <samyak15.sj@gmail.com>
2024-10-03 23:07:42 +0530

    added assets and themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {Inter, Space_Grotesk} from 'next/font/google'
+import type { Metadata } from 'next';
+
 import {
   ClerkProvider,
   SignInButton,
@@ -7,22 +10,51 @@ import {
   UserButton
 } from '@clerk/nextjs'
 import './globals.css'
+import { ThemeProvider } from '@/context/ThemeProvider';
+const inter =  Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
+
+const spaceGrotesk =  Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700',],
+  variable: '--font-spaceGrotesk',
+})
+
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description: 'DevFlow is a platform for developers to ask questions and share knowledge.',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  }
+}
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        }
+      }}
+    >
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
+        <body className={` ${inter.variable} ${spaceGrotesk.variable}`}>
+          <ThemeProvider>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
